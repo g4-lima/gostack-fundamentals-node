f9dafd19c70209e2a931ec87e5d42c1aaa04a3f9
Gabriel Lima <gabastos.lima@gmail.com>
2020-08-26 14:18:25 -0300

    challange complete

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const groupBy = (array: Transaction[], key: string) => {
+      return array.reduce((result: any, currentValue: any) => {
+        // eslint-disable-next-line no-param-reassign
+        (result[currentValue[key]] = result[currentValue[key]] || []).push(
+          currentValue,
+        );
+        return result;
+      }, {});
+    };
+
+    const objectsByType = groupBy(this.transactions, 'type');
+
+    const income = objectsByType.income
+      .map((inc: Transaction) => inc.value)
+      .reduce((prev: number, next: number) => prev + next);
+
+    const outcome = objectsByType.outcome
+      .map((out: Transaction) => out.value)
+      .reduce((prev: number, next: number) => prev + next);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
